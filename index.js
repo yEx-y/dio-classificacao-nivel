0bc0430944a3c3a19c102a9c07a432eae317790a
yEx-y <emer.lopes09@hotmail.com>
2024-06-10 21:13:46 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let listHeroi = ["HEROI", 7001];
+
+if (listHeroi[1] <= 1000) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Ferro")
+
+} else if (listHeroi[1] >= 1001 && listHeroi[1] <= 2000) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Bronze")
+
+} else if (listHeroi[1] >= 2001 && listHeroi[1] <= 5000) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Prata")
+
+} else if (listHeroi[1] >= 5001 && listHeroi[1] <= 7000) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Ouro")
+
+} else if (listHeroi[1] >= 7001 && listHeroi[1] <= 8000) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Platina")
+
+} else if (listHeroi[1] >= 8001 && listHeroi[1] <= 9000) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Ascendente")
+
+} else if (listHeroi[1] >= 9001 && listHeroi[1] <= 10000) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Imortal")
+
+} else if (listHeroi[1] >= 10001) {
+    console.log("O Herói de nome " + listHeroi[0] + " esta no nível de Radiante")
+
+}
